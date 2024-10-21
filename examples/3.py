a4dfe18f86a46915f9c1a0b20020b1fc4995edca
Sak0shi <Sak0shi@vk.com>
2024-10-21 19:32:42 +0500

    Create 3.py

---
+++ examples/3.py
@@ -0,0 +1,9 @@
+def example(color):
+    if color == "green":
+        return "This is grass"
+    elif color == "blue":
+        return "This is water"
+      else:
+        return "I don't know"
+what_is_it = example('red')
+print(what_is_it)
